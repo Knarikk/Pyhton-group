2839ddd61ddbdab689a11ca8a7db44c6d64f75f3
Knarikk <qnarik.stepanyan@polytechnic.am>
2022-03-28 20:36:19 +0400

    New

--- test1.py
+++ test1.py
@@ -1,6 +1,35 @@
-x = 9
-print(x)
-y = 10
-print(y)
-z = 15
-print(z)
+# x = 9
+# print(x)
+# y = 10
+# print(y)
+# z = 15
+# print(z)
+
+# res = []
+# list1 = [1,2,3,5,7,6,3,2,5,10,15,25,20,19,17]
+# for i in range(1,len(list1)-1):
+   
+    
+#     if (list1[i-1] < list1[i] and list1[i] > list1[i+1]) or list1[i-1] > list1[i] and list1[i] < list1[i+1]:
+#         res.append(list1[i])
+# print(res)
+
+# mydict = {
+#     'D':56,
+#     'E':12,
+#     'F':69,
+#     'C':45,
+#     'B':23,
+#     'A':67
+
+# }
+
+# dict1 = {}
+
+# list1 = sorted(mydict,key = mydict.get,reverse = True)[:3]
+# print(list1)    
+# list2 = sorted(mydict.values(),reverse=True)[:3]
+# print(list2)
+# for x,y in zip(list1,list2):
+#     dict1.setdefault(x,y)
+# print(dict1)
