fcc934a5cdd7dacda7c251ae3ed2d44bee3b8681
Knarikk <qnarik.stepanyan@polytechnic.am>
2022-03-31 19:55:46 +0400

    New

---
+++ Homework15.py
@@ -0,0 +1,78 @@
+'''N1'''
+# def calculator(a,b,operation):
+#     if operation == '+':
+#         return a + b
+#     elif operation == '-':
+#         return a - b
+#     elif operation == '*':
+#         return a * b
+#     if operation == '/':
+#         return a / b
+# a = int(input())
+# operation = input()
+# b = int(input())
+# print(calculator(a,b,operation))
+
+'''N2'''
+
+# def maximum(a,b):
+#     if a > b:
+#         print('Maximum is ',a)
+#     else:
+#         print('Maximum is ',b)
+# a = int(input())
+# b = int(input())
+# maximum(a,b)
+    
+'''N3'''
+
+# def func(mylist):
+#     sum = 0
+#     for i in mylist:
+#         sum += i
+#     return sum
+# mylist = [1,2,3,4]
+# print(func(mylist))
+'''N4'''
+
+# def func(mylist):
+#     mul = 1
+#     for i in mylist:
+#         mul *= i
+#     return mul
+# mylist = [1,2,3,4]
+# print(func(mylist))
+
+'''N5'''
+
+# def count(string):
+#     count_alpha = 0
+#     count_digit = 0
+#     for i in string:
+#         if i.isalpha():
+#             count_alpha += 1
+#         else:
+#             count_digit += 1
+#     print('count_alpha = ',count_alpha)
+#     print('count_digit = ',count_digit)
+    
+# string = 'python123'
+# count(string)
+
+'''N6'''
+
+# def function(age1,age2,age3):
+#     for i in res:
+#         if i < 16:
+#             print(i,"Too young! ")
+#             break
+#         else:
+#             print(i,"Get ready!")
+# res =[]
+# age1 = int(input())
+# age2 = int(input())
+# age3 = int(input())
+# res.append(age1)
+# res.append(age2)
+# res.append(age3)
+# function(age1,age2,age3)
--- test1.py
+++ test1.py
@@ -33,3 +33,82 @@
 # for x,y in zip(list1,list2):
 #     dict1.setdefault(x,y)
 # print(dict1)
+
+
+#lesson14
+# def calculator(a,b,gorc):
+#     if gorc == '+':
+#         return a+b
+#     elif gorc == '-':
+#         return a - b
+#     elif gorc == '/':
+#         return a / b
+#     elif gorc == '*':
+#         return a * b
+
+# a = int(input('a= '))
+# b = int(input('b= '))
+# gorc = input()
+# print(calculator(a,b,gorc))
+
+
+# def func(mylist):
+#     sum = 0
+#     for i in mylist:
+#         sum += i
+#     return sum
+# mylist = [1,2,3,4]
+# print(func(mylist))
+
+
+# def func(mylist):
+#     mul = 1
+#     for i in mylist:
+#         mul *= i
+#     return mul
+# mylist = [2,3,4]
+# print(func(mylist))
+
+
+# def func(string):
+#     count = 0
+#     sum = 0
+#     for i in string:
+#         if i.isdigit():
+#             count += 1
+#             sum += int(i)
+#     print(' count= ',count)
+#     print('sum = ',sum)
+
+# string = 'deds1232'
+# func(string)
+
+# x =lambda a,b:a+b
+# print(x(5,2))
+
+
+
+
+# def decorator_func(x):
+#     def gorcoxutyun(list1):
+#         return[x(i[0],i[1]) for i in list1]
+#     return gorcoxutyun
+
+# @decorator_func
+# def func(a,b):
+#     return a+b
+# print(func([(5,3),(7,2)]))
+
+
+# def  fib(n):
+#     if n == 0:
+#         return 0
+#     if n == 1:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+# print(fib(9))
+
+
+#dekoratory funkcia a vory yndgrkum e ayl funkcia kody yndlaynelu hamar
+#inch e funkcian, rekursian
